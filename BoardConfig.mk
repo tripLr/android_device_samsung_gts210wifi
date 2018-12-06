6a18d48a1f777af56eb39e50ca2ea452d4226392
bonuzzz <bonuzzz@gmail.com>
2018-12-06 21:58:15 +0100

    Create lineage-15.1 branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,16 @@
-# Inherit common board flags
-include device/samsung/gts2-common/BoardConfigCommon.mk
-
 LOCAL_PATH := device/samsung/gts210wifi
 
 # Include path
-TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := cm_exynos5433-gts210wifi_defconfig
+TARGET_KERNEL_CONFIG := lineage_gts210wifi_defconfig
+
+# Properties
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts210wifi,gts210wifixx
-RECOVERY_TOUCHSCREEN_SWAP_XY := true
-RECOVERY_TOUCHSCREEN_FLIP_Y := true
-TW_THEME := landscape_hdpi
 
-# SELinux
-BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+# Inherit common board flags
+include device/samsung/gts2-common/BoardConfigCommon.mk
--- lineage.mk
+++ lineage.mk
@@ -6,20 +6,16 @@ $(call inherit-product, device/samsung/gts2-common/device-common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-#$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 ## Device identifier. This must come after all inclusions
-# New name, to be used per lineage-15 as default
 PRODUCT_NAME := lineage_gts210wifi
-# Old name for 14.x tree
-# Reuse this when automated build isn't adapted for lineage_
-# product name before this device goes nightly
-#PRODUCT_NAME := cm_gts210wifi
 PRODUCT_DEVICE := gts210wifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T810
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+BUILD_FINGERPRINT := samsung/gts210wifixx/gts210wifi:7.0/NRD90M/T810XXU2DRB1:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=gts210wifi
