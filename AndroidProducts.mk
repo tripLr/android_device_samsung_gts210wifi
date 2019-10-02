ratmole/android_device_samsung_treltexx

treltexx: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Co-Authored-By: Jan Altensen <info@stricted.net>

AndroidProducts.mk
@@ -15,4 +15,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_treltexx.mk \
-    $(LOCAL_DIR)/rr_treltexx.mk    
+    $(LOCAL_DIR)/rr_treltexx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_treltexx-user \
+    lineage_treltexx-userdebug \
+    lineage_treltexx-eng \
+    rr_treltexx-userdebug

tripLr/android_device_samsung_gts210wifi

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_gts210wifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gts210wifi-user \
+    lineage_gts210wifi-userdebug \
+    lineage_gts210wifi-eng